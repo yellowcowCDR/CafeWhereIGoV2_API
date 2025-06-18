9ad971acf4a764ee8c3ca50e1d9e63d5d31acd06
yellowcowCDR <eofus4250@naver.com>
2025-06-18 20:15:57 +0900

    initial commit

---
+++ src/main/kotlin/com/yellowcow/cafewhereigo/CafeWhereIGoApplication.kt
@@ -0,0 +1,11 @@
+package com.yellowcow.cafewhereigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeWhereIGoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CafeWhereIGoApplication>(*args)
+}
